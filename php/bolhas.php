33436356c002b7dbbb3e762bdda024beb72a7c31
dagdev <dev@dag.hostzi.com>
2015-10-21 08:47:06 -0200

    [Rodrigo Sene] Ajustes para nova base de dados hostinger

--- php/bolhas.php
+++ php/bolhas.php
@@ -1,7 +1,7 @@
 <?php
  
   //Create a connection to the database
-  $mysqli = new mysqli("mysql11.000webhost.com", "a5004318_dag", "mx1Yx2sx3Qx4lx5", "a5004318_dag");
+  $mysqli = new mysqli("mysql.hostinger.com.br", "u371072401_dag", "mx1Yx2sx3Qx4lx5", "u371072401_dag");
 
   if ($mysqli->connect_errno) {
     echo "Falha na conexão com o MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -10,7 +10,7 @@
   //The default result to be output to the browser
   $result = "{'success':false}";
  
-  $query = "SELECT id_bolha, nome, latitude, longitude FROM dag_bolha";
+  $query = "SELECT id_bolha, nome, descricao, latitude, longitude FROM dag_bolha";
 
   //Run the query
   $dbresult = $mysqli->query($query);
@@ -23,6 +23,7 @@
     $markers[] = array(
       'id_bolha'  => $row['id_bolha'],
       'nome'      => $row['nome'],
+      'descricao' => $row['descricao'],
       'latitude'  => $row['latitude'],
       'longitude' => $row['longitude']
     );
@@ -40,7 +41,7 @@
   //Set these headers to avoid any issues with cross origin resource sharing issues
   header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-  header('Access-Control-Allow-Headers: Content-Type,x-prototype-version,x-requested-with');
+  header('Access-Control-Allow-Headers: Content-Type,x-prototype-version,x-requested-with, x-bearer-token');
  
   //Output the result to the browser so that our Ionic application can see the data
   echo($result);
