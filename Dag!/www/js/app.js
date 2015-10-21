ecbe83b6a0e218b899a68bd44ede8ee77b0317ab
dagdev <dev@dag.hostzi.com>
2015-10-21 17:09:30 -0200

    [Rodrigo Sene] Ajuste para criar marcadores (Bolhas) em todos os lugares que estão cadastrados no banco de dados, e utilizar o icone de acordo com o tipo.

--- Dag!/www/js/app.js
+++ Dag!/www/js/app.js
@@ -5,7 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, GoogleMaps) {
+  console.log('[IN] run()');
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,11 +16,12 @@ angular.module('starter', ['ionic', 'ngCordova'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-  });
+  })
+  GoogleMaps.init();
 })
 
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
- 
+  //console.log("tt");
   // Força o tabs no rodapé utilizando o Android
   $ionicConfigProvider.tabs.position('bottom')
 
@@ -29,12 +31,6 @@ angular.module('starter', ['ionic', 'ngCordova'])
     templateUrl: 'templates/map.html',
     controller: 'MapCtrl'
   })
-
-  .state('tabs', {
-    url: '/tab',
-    abstract: true
-  })
-  
   .state('tabs.chat', {
     url: '/chat',
     views: {
@@ -48,42 +44,126 @@ angular.module('starter', ['ionic', 'ngCordova'])
  
 })
 
-.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
+.factory('Markers', function($http) {
  
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true
-    };
- 
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var markers = [];
 
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
- 
-      var marker = new google.maps.Marker({
-          map: $scope.map,
-          animation: google.maps.Animation.DROP,
-          position: latLng,
-          icon: 'img/marker.png'
-      });    
+  return {
+    getMarkers: function(){
 
-      var infoWindow = new google.maps.InfoWindow({
-          content: "Dag Ball"
+      console.log("Entrou Markers.getMarkers()");
+
+       return $http.get("http://dagball.xyz/testes/bolhas.php").then(function(dados){
+
+        markers = dados;
+        
+        return markers;
+        
       });
+    }
+  }
+})
 
-      google.maps.event.addListener(marker, 'click', function () {
-          infoWindow.open($scope.map, marker);
+.factory('GoogleMaps', function($cordovaGeolocation, Markers){
+ 
+  var apiKey = false;
+  var map = null;
+ 
+  function initMap(){
+    console.log("[IN] initMap()")
+    var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+      };
+      console.log("Chegou aqui 1")
+      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+     
+      //Wait until the map is loaded
+      google.maps.event.addListenerOnce(map, 'idle', function(){
+ 
+        //Load the markers
+        console.log("Chamou loadMarkers()")
+        loadMarkers();
+        
       });
-
+ 
+    }, function(error){
+      console.log("Could not get location");
+ 
+        //Load the markers
+        loadMarkers();
     });
-  }, function(error){
-    console.log("Could not get location");
-  });
+ 
+  }
+ 
+  function loadMarkers(){
+      //Get all of the markers from our Markers factory
+      Markers.getMarkers().then(function(markers){
 
+        var iconBase = 'img/';
+
+        var  icons = {
+          Esporte:{ icon: iconBase + 'Esporte.png'},
+          Noite:  { icon: iconBase + 'Noite.png'},
+          Estudo: { icon: iconBase + 'Estudo.png'}
+        };
+
+        var records  = markers.data;
+
+        for (var i = 0; i < records.length; i++) {
+ 
+          var record = records[i];   
+          console.log(record.latitude);
+          console.log(record.longitude);
+          var markerPos = new google.maps.LatLng(record.latitude, record.longitude);
+          console.log(record.tp);
+          // Add the markerto the map
+          var marker = new google.maps.Marker({
+              map: map,
+              animation: google.maps.Animation.DROP,
+              position: markerPos,
+              icon: icons[record.tipo].icon
+          });   
+          console.log(marker);
+          var infoWindowContent = "<h4>" + record.nome + "</h4> <BR/> <h5>"+ record.descricao +"</h5>" ;          
+ 
+          addInfoWindow(marker, infoWindowContent, record);
+ 
+        }
+ 
+      }); 
+ 
+  }
+ 
+  function addInfoWindow(marker, message, record) {
+ 
+      var infoWindow = new google.maps.InfoWindow({
+          content: message
+      });
+ 
+      google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open(map, marker);
+      });
+ 
+  }
+ 
+  return {
+    init: function(){
+      console.log("Chamou initMap");
+      initMap();
+    }
+  }
+ 
+})
+
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+  
 });
